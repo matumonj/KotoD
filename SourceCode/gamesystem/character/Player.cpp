899bbc002d4399d63dfa1447fef67f8f3e500cfd
shimadatuyoshi <k020g1186@g.neec.ac.jp>
2023-07-31 23:51:20 +0900

    ちょいとフラグの追加

--- SourceCode/gamesystem/character/Player.cpp
+++ SourceCode/gamesystem/character/Player.cpp
@@ -250,7 +250,7 @@ void Player::Draw(DirectXCommon* dxCommon)
 	BulletDraw(attackbullets, dxCommon);
 	IKEObject3d::PreDraw();
 	skirtobj->Draw();
-	if (m_viewBull) {
+	if (m_viewBull&&display) {
 		sutoobj->Draw();
 	}
 	IKEObject3d::PostDraw();
@@ -1078,4 +1078,13 @@ void Player::TitleUpdate() {
 
 	playerattach->TitleUpdate();
 	SetParam();
+}
+
+void Player::TyutorialUpdate()
+{
+	fbxmodels->GetBoneIndexMat(index, skirtmat);
+	skirtobj->FollowUpdate(skirtmat);
+	skirtobj->SetColor(m_Color);
+	SetParam();
+	display = false;
 }
